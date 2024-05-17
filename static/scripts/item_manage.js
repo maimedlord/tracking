ccb5a53478b7bacca3d7e2b58c4fee37b9f0f9f2
maimedlord <52831314+maimedlord@users.noreply.github.com>
2024-05-17 00:40:56 -0400

    fix graph buttons item_manage

--- static/scripts/item_manage.js
+++ static/scripts/item_manage.js
@@ -1,5 +1,7 @@
 // global variables
 const API_URL = 'http://127.0.0.1:5000/item_doc_refresh_list/' + g_item_name;
+const BTN_CLR_OFF = 'deepskyblue';
+const BTN_CLR_ON = 'grey';
 const CANVAS_ID = 'item_graph_canvas';
 const DATE_TODAY = new Date();
 const DATE_LAST_MONTH = new Date().setDate(DATE_TODAY.getDate() - 31);
@@ -43,7 +45,7 @@ function get_item_docs() {
                 // pull data array out of response object and deep copy to ITEM_DOCS
                 ITEM_DOCS = structuredClone(data['data']);
                 //
-                if (ITEM_DOCS.length < 2) {
+                if (ITEM_DOCS.length < 2) { // PROBABLY GOING TO HAVE A PROBLEM HERE WHEN ADDING VIEWS_* !!!!!!!!!!!!!!!!!!!!!!
                     return;
                 }
             }
@@ -59,7 +61,7 @@ function get_item_docs() {
                 }
                 DIV_LIST_ITEM_DOCS.append(temp_div);
             }
-            /// draw chart
+            /* draw chart */
             // prepare data object for chart
             GRAPH_DATA_OBJECT = {
                 datasets: [
@@ -139,12 +141,16 @@ function draw_bubble_graph(canvas_id, data_objects, title_text, x_max, x_min) {
     );
 }
 
-//
-function redraw_graph_buttons() {
-    console.log(DIV_BUTTONS.children.item(0));
-    for (let i = 0; i <= DIV_BUTTONS.children.length; i++) {
-        //DIV_BUTTONS.children.item(i).className = 'graph_buttons';
+// redraw graph buttons
+function redraw_graph_buttons(div_id) {
+    elements = document.getElementsByClassName('graph_buttons');
+    for (let i = 0; i <= elements.length; i++) {
+        if (elements[i]) {
+            console.log(elements[i].innerHTML);
+            elements[i].style.backgroundColor = BTN_CLR_OFF;
+        }
     }
+    document.getElementById(div_id).style.backgroundColor = BTN_CLR_ON;
 }
 
 /*
@@ -152,27 +158,26 @@ function redraw_graph_buttons() {
  */
 
 document.getElementById('graph_button_all_time').onclick=function() {
-    this.style.backgroundColor = 'grey';
-    redraw_graph_buttons();
+    redraw_graph_buttons(this.id);
     draw_bubble_graph(CANVAS_ID, GRAPH_DATA_OBJECT, 'for all time', null, null);
 }
 document.getElementById('graph_button_all_time_today').onclick=function () {
-    this.style.backgroundColor = 'grey';
+    redraw_graph_buttons(this.id);
     draw_bubble_graph(CANVAS_ID, GRAPH_DATA_OBJECT, 'for all time to today', DATE_TOMORROW, null);
 }
 document.getElementById('graph_button_last_month').onclick=function () {
-    this.style.backgroundColor = 'grey';
+    redraw_graph_buttons(this.id);
     draw_bubble_graph(CANVAS_ID, GRAPH_DATA_OBJECT, 'for last month', DATE_TOMORROW, DATE_LAST_MONTH);
 }
 document.getElementById('graph_button_last_qtr').onclick=function () {
-    this.style.backgroundColor = 'grey';
+    redraw_graph_buttons(this.id);
     draw_bubble_graph(CANVAS_ID, GRAPH_DATA_OBJECT, 'for last quarter', DATE_TOMORROW, DATE_LAST_QTR);
 }
 document.getElementById('graph_button_last_week').onclick=function () {
-    this.style.backgroundColor = 'grey';
+    redraw_graph_buttons(this.id);
     draw_bubble_graph(CANVAS_ID, GRAPH_DATA_OBJECT, 'for last week', DATE_TOMORROW, DATE_LAST_WEEK);
 }
 document.getElementById('graph_button_last_year').onclick=function () {
-    this.style.backgroundColor = 'grey';
+    redraw_graph_buttons(this.id);
     draw_bubble_graph(CANVAS_ID, GRAPH_DATA_OBJECT, 'for last year', DATE_TOMORROW, DATE_LAST_YEAR);
 }
